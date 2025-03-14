835018cfbfc82d77cd06170b030cedd2532e2892
Arnau del Río <arnau.delrio@gmail.com>
2025-03-14 13:29:14 +0100

    Updating nvim config code comments

--- nvim/lua/setup/markdown.lua
+++ nvim/lua/setup/markdown.lua
@@ -17,4 +17,15 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
+  {
+    'SleepySwords/mathjax.nvim',
+    -- Replace this with whatever node package manager you use.
+    build = 'cd mathjax && yarn install',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      '3rd/image.nvim',
+    },
+    opts = {},
+  },
 }
