ee33382dc787b63a8b119fcca82bd17b03aa4c25
Arnau del Río <arnau.delrio@gmail.com>
2025-03-14 13:28:20 +0100

    Configuring background functionality and adding a background

--- hypr/backgroundChange.sh
+++ hypr/backgroundChange.sh
@@ -12,7 +12,7 @@ directory="/home/Al4st0r/dotfiles/hypr/BackgroundImages/"
 monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
 
 if [ -d "$directory" ]; then
-    random_background=$(ls $directory*.jpg | shuf -n 1)
+    random_background=$(ls $directory* | shuf -n 1)
     hyprctl hyprpaper unload all
     hyprctl hyprpaper preload $random_background
     hyprctl hyprpaper wallpaper "$monitor,contain:$random_background"
